ce3232d3584a17e3615488a1f74dd23ef814318a
slinxxy <slinxxy@gmail.com>
2023-08-11 21:18:23 +0200

    reorder files

--- src/Controller/TrainingcenterController.php
+++ src/Controller/TrainingcenterController.php
@@ -1,18 +1,25 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\hello\Controller\HelloController.
- */
-namespace Drupal\Trainingcenter\Controller;
+namespace Drupal\trainingcenter\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
+/**
+ * Returns responses for Trainingcenter routes.
+ */
 class TrainingcenterController extends ControllerBase {
-  public function content() {
-    return array(
-      '#type' => 'markup',
-      '#markup' => t('Hello, World!'),
-    );
+
+  /**
+   * Builds the response.
+   */
+  public function build() {
+
+    $build['content'] = [
+      '#type' => 'item',
+      '#markup' => $this->t('It works!'),
+    ];
+
+    return $build;
   }
+
 }
